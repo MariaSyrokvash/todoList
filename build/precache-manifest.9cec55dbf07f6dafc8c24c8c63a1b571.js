6471d60b10ce4c87da6d41e0517f1cbccd2cbcd2
MariaSyrokvash <mariya.syrokvash@gmail.com>
2021-03-18 17:40:01 +0300

    feat: add deploy

---
+++ build/precache-manifest.9cec55dbf07f6dafc8c24c8c63a1b571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc179416c5a6819d67c253cc14c8de9",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "5e5f8a29b4bcde656ef4",
+    "url": "/todoList/static/css/main.309d6a3e.chunk.css"
+  },
+  {
+    "revision": "3c7c41f2c60989ec21ca",
+    "url": "/todoList/static/js/2.2f4b163b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoList/static/js/2.2f4b163b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5f8a29b4bcde656ef4",
+    "url": "/todoList/static/js/main.67da56e9.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
